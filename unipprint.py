e0277519a0e2a708387388461779c8f35bce96e4
root <root@discuz.(none)>
2014-09-03 11:39:46 -0400

    init

---
+++ unipprint.py
@@ -0,0 +1,12 @@
+# coding=utf8
+
+import pprint
+
+class MyPrettyPrinter(pprint.PrettyPrinter):
+    def format(self, object, context, maxlevels, level):
+        if isinstance(object, unicode):
+            return (object.encode('utf8'), True, False)
+        return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
+
+def unipprint(object):
+    MyPrettyPrinter().pprint(object)
